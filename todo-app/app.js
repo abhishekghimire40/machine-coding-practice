2a5c27b9309e2db1e456200317aff2ebfc1948db
Abhishek Ghimire <abhishek@pop-os.localdomain>
2023-06-09 14:25:52 +0545

    remove todo on click featuer added

--- todo-app/app.js
+++ todo-app/app.js
@@ -4,14 +4,31 @@ const addTodoBtn = document.querySelector(".add-todo");
 const todoContainer = document.querySelector(".list-todo");
 const clearForm = document.querySelector(".form i");
 
-const allTodo = [];
-listTodo(allTodo);
+let allTodo = [];
+
+(function () {
+  allTodo = getTodo();
+  listTodo(allTodo);
+})();
+
+// function to store todo in local storage
+function storeTodo(todo_list) {
+  localStorage.setItem("todo", JSON.stringify(todo_list));
+}
+
+// function to get todo from local storage if any
+function getTodo() {
+  if (localStorage.getItem("todo")) {
+    return JSON.parse(localStorage.getItem("todo"));
+  }
+  return [];
+}
 
 // function to clear input field
 function clearInput() {
   formInput.value = "";
 }
-
+// list all added todos of user
 function listTodo(todo) {
   if (todo.length === 0) {
     todoContainer.innerHTML = `
@@ -20,11 +37,10 @@ function listTodo(todo) {
     return;
   }
   todoContainer.innerHTML = todo
-    .filter((el) => !el.remove)
     .map((el, i) => {
       return `
     <div data-id=${i}  class="todo">
-      <p>${el.text}</p>
+      <p>${el}</p>
       <i class="fa-solid fa-x"></i>
     </div>
     `;
@@ -37,10 +53,24 @@ function addTodo(e) {
   if (!formInput.value) return;
   const formValue = formInput.value;
   clearInput();
-  allTodo.push({ text: formValue, remove: false });
+  allTodo.push(formValue);
+  storeTodo(allTodo);
   listTodo(allTodo);
+}
+
+function removeSingleTodo(id) {
+  allTodo = allTodo.filter((todo, i) => i != id);
+  storeTodo(allTodo);
+  listTodo(allTodo);
+}
+
+function deleteTodo(e) {
+  if (e.target.tagName !== "I") return;
+  const todo_div = e.target.closest(".todo");
+  removeSingleTodo(todo_div.dataset.id);
 }
 
 // Event listeners
 clearForm.addEventListener("click", clearInput);
 addTodoBtn.addEventListener("click", addTodo);
+todoContainer.addEventListener("click", deleteTodo);

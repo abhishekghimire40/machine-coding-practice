7c1c07d13788c46ed9a4325214a7a496dc4d6856
Abhishek Ghimire <abhishek@pop-os.localdomain>
2023-06-11 16:46:08 +0545

    refactored pagination again just to show First,Prev, current page and Last page

--- pagination/src/components/Paginate.jsx
+++ pagination/src/components/Paginate.jsx
@@ -1,23 +1,47 @@
+// paginate function to show first,prev, current,next,last pagination
 const Paginate = ({ page, pageHandler, products }) => {
   return (
     <div className="pagination">
-      {page > 1 && <span onClick={() => pageHandler(page - 1)}>Prev</span>}
-      {[...Array(products.length)].map((_, i) => {
-        return (
-          <span
-            className={page === i + 1 ? "active" : ""}
-            onClick={() => pageHandler(i + 1)}
-            key={i}
-          >
-            {" "}
-            {i + 1}{" "}
-          </span>
-        );
-      })}
-      {page < 10 && <span onClick={() => pageHandler(page + 1)}>Next</span>}
+      {page > 1 && <span onClick={() => pageHandler(1)}>First</span>}
+
+      {page > 2 && (
+        <span onClick={() => pageHandler(page - 1)}>{page - 1}</span>
+      )}
+      <span className="active" onClick={() => pageHandler(page)}>
+        {page}
+      </span>
+      {page < 9 && (
+        <span onClick={() => pageHandler(page + 1)}>{page + 1}</span>
+      )}
+
+      {page < 10 && (
+        <span onClick={() => pageHandler(products.length)}>Last</span>
+      )}
     </div>
   );
 };
+
+// pagination to show all Prev, all pages...(1,2,3,4,5,6,7,8,9,10),Next
+// const Paginate = ({ page, pageHandler, products }) => {
+//   return (
+//     <div className="pagination">
+//       {page > 1 && <span onClick={() => pageHandler(page - 1)}>Prev</span>}
+//       {[...Array(products.length)].map((_, i) => {
+//         return (
+//           <span
+//             className={page === i + 1 ? "active" : ""}
+//             onClick={() => pageHandler(i + 1)}
+//             key={i}
+//           >
+//             {" "}
+//             {i + 1}{" "}
+//           </span>
+//         );
+//       })}
+//       {page < 10 && <span onClick={() => pageHandler(page + 1)}>Next</span>}
+//     </div>
+//   );
+// };
 
 export default Paginate;
 
